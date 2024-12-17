a61f08fcc048d8bc6b02b00d241157bc13774a4c
nalinimano <nalinimano@gmail.com>
2024-12-17 22:32:50 +0530

    added day9 Json parsing program

---
+++ src/main/java/day9_JsonCreate/ExcelToJson.java
@@ -0,0 +1,17 @@
+package day9_JsonCreate;
+
+import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonArrayFormatVisitor;
+import org.testng.annotations.Test;
+
+public class ExcelToJson {
+
+    @Test
+    public void runner()
+    {
+        // Input Excel file path and Output JSON file path
+        String inputexl="src/main/inputs/input.xlsx";
+        String jsonFilePath="src/main/inputs/jsonfiles/output.json";
+
+
+    }
+}
