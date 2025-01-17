29e85531631ef1ff0d596d942851a8bbb05fc09d
nalinimano <nalinimano@gmail.com>
2025-01-17 22:37:09 +0530

    revision_day6

---
+++ src/main/java/other/InterviewQuestion2.java
@@ -0,0 +1,13 @@
+package other;
+
+public class InterviewQuestion2 {
+    public static void main(String[] args) {
+
+        \u000dSystem.out.println("Try programiz.pro");
+
+        System.out.println("test1");
+
+
+        /*** '\u000d  unicode escape sequence carriage return' **/
+    }
+}
---
+++ src/main/java/other/InterviewQuestions.java
@@ -0,0 +1,16 @@
+package other;
+
+public class InterviewQuestions {
+
+    public static void main(String[] args) {
+        if(true)
+        {
+          //  break;//break should be used inside loop & switch statement
+                  // else we get error "break outsie switch or loop"
+        }
+
+
+
+        System.out.println("Test");
+    }
+}
